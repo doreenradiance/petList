00985b2bfca0d3886aaea874eabdc22b1aa3eee6
Doreen Mensah <doreenradiance@gmail.com>
2020-11-20 16:46:04 +0000

    Appied The Pet Screen

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import petsScreen from './petScreen';
+import SignUpScreen from "./SignUpScreen"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+      <SignUpScreen/>
+    //  <petsScreen/>
+      // <Pets/> 
+   
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 100,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "space-around",
   },
 });
