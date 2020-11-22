07a14cbb6272f97ef2ca6673ed7da9b7ec3ae97e
Doreen Mensah <doreenradiance@gmail.com>
2020-11-22 12:46:38 +0000

    Made Changes With Styles

--- components/Pets.js
+++ components/Pets.js
@@ -1,12 +1,76 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import { SimpleLineIcons } from '@expo/vector-icons';
 
-export default function Pets() {
+export default function pets({ name, age, location, image }) {
     return (
-        <View>
-            <Text></Text>
+        <View style={styles.container}>
+            <Image source={image} style={styles.image} />
+
+            <View style={styles.infoContainer}>
+                <Text style={styles.name} numberOfLines={1}>{name}</Text>
+                <Text style={styles.dog}>Dog</Text>
+                <Text style={styles.age}>{age}</Text>
+                <View style={styles.icon}>
+                    <SimpleLineIcons name="location-pin" size={24} color="black" />
+                    <Text style={styles.location} numberOfLines={1}>{location}</Text>
+                </View>
+            </View>
+
+
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "row",
+        marginVertical: 10,
+    },
+
+    image: {
+        width: 150,
+        height: 190,
+        borderRadius: 10,
+        marginLeft: 25
+    },
+
+    infoContainer: {
+        backgroundColor: "white",
+        borderTopRightRadius: 10,
+        borderBottomRightRadius: 10,
+        height: 120,
+        width: 150,
+        marginTop: 30,
+
+    },
+
+    name: {
+        color: "#5b038f",
+        fontSize: 20,
+        marginLeft: 20,
+        marginTop: 10
+    },
+
+    dog: {
+        fontSize: 15,
+        marginLeft: 20,
+    },
+
+    age: {
+        fontSize: 15,
+        marginLeft: 20,
+        marginBottom: 5
+    },
+
+    location: {
+        fontSize: 15,
+        marginLeft: 5,
+    },
+
+    icon: {
+        flexDirection: "row",
+        marginLeft: 15
+    }
+})
